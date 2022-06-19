c3a0db1b82cd40314f0226c9e4003d0194de3476
sukanyapavuluri33 <101783413+sukanyapavuluri33@users.noreply.github.com>
2022-06-19 03:51:45 -0700

    Create Frequency.py

---
+++ Frequency.py
@@ -0,0 +1,36 @@
+def printChar(arr, Len):
+ 
+ 
+    occ = {}
+    for i in range(Len):
+         
+        if(arr[i] in occ):
+            occ[arr[i]] = occ[arr[i]] + 1
+         
+        else:
+            occ[arr[i]] = 1
+  
+   
+    size = len(occ)
+
+    while (size > 0):
+        
+        currentMax = 0
+        arg_max = 0
+        for key, value in occ.items():
+ 
+            if (value > currentMax or (value == currentMax and key > arg_max)):
+                arg_max = key
+                currentMax = value
+  
+        print(f"{arg_max} - {currentMax}")
+ 
+        occ.pop(arg_max)
+        size -= 1
+ 
+
+Str =input("please enter a string ")
+Len = len(Str)
+ 
+ 
+printChar(list(Str), Len)
